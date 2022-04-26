03260d0d66f4d605a1238f028e1a81f402745710
Ha <khanhdb@hantt.local>
2022-04-27 02:41:58 +0700

    add file lessonVmodel

---
+++ src/components/LessonVmodel.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <input id="task" type="text" v-model="task"/>
+        <p>Da nhap vao: {{task}}</p>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return({
+            task: ''
+        })
+    },
+}
+</script>
