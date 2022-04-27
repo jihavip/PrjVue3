12f0d0ed08697e11c98bd0be2002995725d1bbda
Ha <khanhdb@hantt.local>
2022-04-27 14:26:20 +0700

    add filr component lessionVmodel

--- src/components/LessonVmodel.vue
+++ src/components/LessonVmodel.vue
@@ -1,15 +1,32 @@
 <template>
     <div>
-        <input id="task" type="text" v-model="task"/>
-        <p>Da nhap vao: {{task}}</p>
+        <p>Nhap vao day: </p><input id="task" type="text" v-model="task"/>
+        <button @click="addTask">add</button>
+        <button @click="remoteTask">delete</button>
+        <br/>
+        <p>List vua nhap: </p>
+        <ul>
+            <li v-for ="todo in todoList" :key="todo">{{todo}}</li>
+        </ul>
+        <p></p>
+        
     </div>
 </template>
 <script>
 export default {
     data() {
         return({
-            task: ''
+           task:'',
+           todoList: []
         })
     },
+    methods:{
+        addTask(){
+            this.todoList.push(this.task)
+        },
+        remoteTask(){
+            this.todoList.pop()
+        }
+    }
 }
 </script>
